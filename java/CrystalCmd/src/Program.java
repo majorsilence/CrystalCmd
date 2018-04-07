f7243a67c8f2ccc5cf6d49f72b507677a7dd3e42
Peter Gill <peter@majorsilence.com>
2018-04-07 17:46:27 -0230

    start work on http service option

--- java/CrystalCmd/src/Program.java
+++ java/CrystalCmd/src/Program.java
@@ -4,6 +4,7 @@ import com.crystaldecisions.sdk.occa.report.application.ReportClientDocument;
 import com.crystaldecisions.sdk.occa.report.definition.IReportObject;
 import com.crystaldecisions.sdk.occa.report.exportoptions.ReportExportFormat;
 import com.crystaldecisions.sdk.occa.report.lib.ReportSDKException;
+import com.sun.net.httpserver.HttpServer;
 
 import jj2000.j2k.NotImplementedError;
 
@@ -12,6 +13,7 @@ import com.crystaldecisions.sdk.occa.report.application.ParameterFieldController
 import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -55,42 +57,17 @@ public class Program {
 			com.google.gson.Gson gson = new com.google.gson.Gson();
 			convertedDataFile = gson.fromJson(datafile, Data.class);
 
-			exportReport(reportpath, outpath, convertedDataFile);
+			PdfExporter pdfExport = new PdfExporter();
+			pdfExport.exportReport(reportpath, outpath, convertedDataFile);
 		} else {
-			System.out.println("Not supported");
-			// TODO: load in data from memory stream passed in
-			throw new NotImplementedError();
-			/*
-			 * String data = ReadConsoleInputForData();
-			 * 
-			 * com.google.gson.Gson gson = new com.google.gson.Gson(); convertedDataFile =
-			 * gson.fromJson(data, Data.class);
-			 * 
-			 * exportReport("/home/peter/Projects/CrystalWrapper/thereport.rpt", outpath,
-			 * convertedDataFile);
-			 */
+			System.out.println("Running in server mode");
+			HttpServer server = HttpServer.create(new InetSocketAddress(4321), 0);
+			server.createContext("/status", new ServerStatus());
+			server.createContext("/export", new ServerExport());
+			server.setExecutor(null); // creates a default executor
+			server.start();
 
 		}
-	}
-
-	private static String ReadConsoleInputForData() {
-		Scanner scanner = new Scanner(System.in); // Use a Scanner.
-		List<String> al = new ArrayList<String>(); // The list of names (String(s)).
-		String word; // The current line.
-		while (scanner.hasNextLine()) { // make sure there is a line.
-			word = scanner.nextLine(); // get the line.
-			if (word != null) { // make sure it isn't null.
-				word = word.trim(); // trim it.
-				if (word.equalsIgnoreCase("done")) { // check for done.
-					break; // End on "done".
-				}
-				al.add(word); // Add the line to the list.
-			} else {
-				break; // End on null.
-			}
-		}
-		scanner.close();
-		return al.toString();
 	}
 
 	private static String readFile(String path, Charset encoding) throws IOException {
@@ -98,104 +75,5 @@ public class Program {
 		return new String(encoded, encoding);
 	}
 
-	private static void exportReport(String reportPath, String outputPath, Data datafile)
-			throws ReportSDKException, IOException, SQLException {
-
-		ReportClientDocument reportClientDocument;
-		ByteArrayInputStream byteArrayInputStream;
-		byte[] byteArray;
-		int bytesRead;
-
-		/*
-		 * Instantiate ReportClientDocument and specify the Java Print Engine as the
-		 * report processor. Open a rpt file and export to PDF. Stream PDF back to web
-		 * browser.
-		 */
-
-		reportClientDocument = new ReportClientDocument();
-
-		reportClientDocument.setReportAppServer(ReportClientDocument.inprocConnectionString);
-
-		reportClientDocument.open(reportPath, OpenReportOptions._openAsReadOnly);
-
-		// Object reportSource = reportClientDocument.getReportSource();
-
-		if (datafile != null) {
-			for (Map.Entry<String, Object> item : datafile.getParameters().entrySet()) {
-				ParameterFieldController parameterFieldController;
-
-				parameterFieldController = reportClientDocument.getDataDefController().getParameterFieldController();
-				parameterFieldController.setCurrentValue("", item.getKey(), item.getValue());
-				/*
-				 * parameterFieldController.setCurrentValue("", "StringParam", "Hello");
-				 * parameterFieldController.setCurrentValue("sub", "StringParam",
-				 * "Subreport string value"); parameterFieldController.setCurrentValue("",
-				 * "BooleanParam", new Boolean(true));
-				 * parameterFieldController.setCurrentValue("", "CurrencyParam", new
-				 * Double(123.45)); parameterFieldController.setCurrentValue("", "NumberParam",
-				 * new Integer(123));
-				 * 
-				 */
-				// rpt.SetParameterValue(item.Key, item.Value);
-			}
-			for (Map.Entry<String, String> item : datafile.getDataTables().entrySet()) {
-
-				CsharpResultSet inst = new CsharpResultSet();
-				ResultSet result = inst.Execute(item.getValue());
-
-				reportClientDocument.getDatabaseController().setDataSource(result, item.getKey(), item.getKey());
-
-			}
-			for (Iterator<MoveObjects> itr = datafile.getMoveObjectPosition().iterator(); itr.hasNext();) {
-				MoveObjects item = itr.next();
-				moveReportObject(reportClientDocument, item);
-			}
-		}
-
-		byteArrayInputStream = (ByteArrayInputStream) reportClientDocument.getPrintOutputController()
-				.export(ReportExportFormat.PDF);
-
-		byteArray = new byte[1024];
-		/*
-		 * while((bytesRead = byteArrayInputStream.read(byteArray)) != -1) {
-		 * response.getOutputStream().write(byteArray, 0, bytesRead); }
-		 */
-		FileOutputStream fos = new FileOutputStream(outputPath);
-		while ((bytesRead = byteArrayInputStream.read(byteArray)) != -1) {
-			fos.write(byteArray, 0, bytesRead);
-		}
-		fos.close();
-
-		reportClientDocument.close();
-	}
-
-	private static void moveReportObject(ReportClientDocument reportClientDocument, MoveObjects item)
-			throws ReportSDKException {
-		IReportObject control = reportClientDocument.getReportDefController().findObjectByName(item.ObjectName);
-
-		if (item.Pos == MovePosition.LEFT) {
-			control.setLeft(item.Move);
-		}
-
-		if (item.Type == MoveType.ABSOLUTE) {
-			switch (item.Pos) {
-			case LEFT:
-				control.setLeft(item.Move);
-				break;
-			case TOP:
-				control.setTop(item.Move);
-				break;
-			}
-		} else {
-			switch (item.Pos) {
-			case LEFT:
-				control.setLeft(control.getLeft() + item.Move);
-				break;
-			case TOP:
-				control.setTop(control.getTop() + item.Move);
-				break;
-			}
-		}
-	}
-
+	
 }
---
+++ java/CrystalCmd/src/ServerExport.java
@@ -0,0 +1,22 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.Headers;
+
+public class ServerExport implements HttpHandler {
+    public void handle(HttpExchange t) throws IOException {
+      String response = "Use /get to download a PDF";
+      t.sendResponseHeaders(200, response.length());
+      OutputStream os = t.getResponseBody();
+      os.write(response.getBytes());
+      os.close();
+    }
+		  
+}
