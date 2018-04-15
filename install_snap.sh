bc2d7bedb07f3859cc362dcbd3130d33c61e03bf
Peter Gill <peter@majorsilence.com>
2018-04-15 12:37:28 -0230

    Include test server c.majorsilence.com

--- install_snap.sh
+++ install_snap.sh
@@ -4,3 +4,6 @@ set -u # exit on using unset variable
 
 
 snap install ./java/CrystalCmd/build/CrystalCmd.snap --force-dangerous --classic # force dangerous is needed because the snap is not signed
+
+scp ./java/CrystalCmd/build/CrystalCmd.snap root@c.majorsilence.com:/root
+ssh root@c.majorsilence.com 'snap install /root/CrystalCmd.snap --force-dangerous --classic'
