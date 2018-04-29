66bf3e17866da3b4186d9fa2b2e2b9499fcb91a6
Peter Gill <peter@majorsilence.com>
2018-04-29 10:43:42 -0230

    new snap version 0.3

--- build_snap.sh
+++ build_snap.sh
@@ -5,12 +5,12 @@ set -u # exit on using unset variable
 
 snap_build()
 {
-	wget http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jre-8u161-linux-x64.tar.gz --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0" --header="Cookie: oraclelicense=accept-securebackup-cookie Accept: text/html"
+	wget http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jre-8u172-linux-x64.tar.gz --user-agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0" --header="Cookie: oraclelicense=accept-securebackup-cookie Accept: text/html"
 
 	snapcraft clean
 	snapcraft snap -o "$(pwd)/java/CrystalCmd/build/CrystalCmd.snap"
 	# snapcraft clean
-	rm -rf ./jre-8u161-linux-x64.tar.gz
+	rm -rf ./jre-8u172-linux-x64.tar.gz
 }
 
 snap_build
