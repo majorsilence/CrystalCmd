941266ee5d2afc08bcfd521788ce673c5c61e0a6
Peter Gill <peter@majorsilence.com>
2020-11-28 21:04:05 -0330

    Add report interface and calling program httpclient management

---
+++ dotnet/Majorsilence.CrystalCmd.Client/IReport.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Majorsilence.CrystalCmd.Client
+{
+    public interface IReport
+    {
+        Task<Stream> GenerateAsync(Data reportData, Stream report);
+        Task<Stream> GenerateAsync(Data reportData, Stream report, HttpClient httpClient);
+    }
+}
--- dotnet/Majorsilence.CrystalCmd.Client/Report.cs
+++ dotnet/Majorsilence.CrystalCmd.Client/Report.cs
@@ -5,45 +5,103 @@ using System.Threading.Tasks;
 
 namespace Majorsilence.CrystalCmd.Client
 {
-    public class Report
+    public class Report : IReport
     {
         readonly string serverUrl;
-        public Report(string serverUrl= "https://c.majorsilence.com/export")
+        readonly string userAgent;
+        public Report(string serverUrl = "https://c.majorsilence.com/export",
+            string userAgent = "Majorsilence.CrystalCmd.Client/1.0.0 Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0")
         {
             this.serverUrl = serverUrl;
+            this.userAgent = userAgent;
         }
 
 
-        public Stream Generate(Data data, string reportPath)
-        {
-            throw new NotImplementedException();
-        }
+        //public Stream Generate(Data data, string reportPath)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Stream Generate(Data data, byte[] report)
-        {
-            throw new NotImplementedException();
-        }
+        //public Stream Generate(Data data, byte[] report)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
+
+        /// <summary>
+        /// Call a server with a crystal report template and data and have a pdf report returned.
+        /// An HttpClient is created disposed on every call to this method.  It is recommended
+        /// to call the GenerateAsync method passing in an HttpClient.
+        /// </summary>
+        /// <param name="reportData"></param>
+        /// <param name="report"></param>
+        /// <returns></returns>
+        /// <example>
+        /// <code lang="cs">
+        /// using (var fstream = new FileStream("thereport.rpt", FileMode.Open))
+        /// using (var fstreamOut = new FileStream("thereport.pdf", FileMode.OpenOrCreate | FileMode.Append))
+        /// {
+        ///     var rpt = new Majorsilence.CrystalCmd.Client.Report();
+        ///     using (var stream = await rpt.GenerateAsync(new Data(), fstream))
+        ///     {
+        ///         stream.CopyTo(fstreamOut);
+        ///     }
+        /// }
+        ///</code>
+        /// </example>
         public async Task<Stream> GenerateAsync(Data reportData, Stream report)
         {
             string json = Newtonsoft.Json.JsonConvert.SerializeObject(reportData);
 
-            HttpClient httpClient = new HttpClient();
-            MultipartFormDataContent form = new MultipartFormDataContent();
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
+                using (var form = new MultipartFormDataContent())
+                {
+                    form.Add(new StringContent(json), "reportdata");
+                    form.Add(new StreamContent(report), "reporttemplate", "report.rpt");
+                    //form.Add(new ByteArrayContent(crystalReport), "reporttemplate", "the_dataset_report.rpt");
+                    HttpResponseMessage response = await httpClient.PostAsync(serverUrl, form);
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStreamAsync();
+                }
+            }
+        }
 
-            form.Add(new StringContent(json), "reportdata");
-            form.Add(new StreamContent(report), "reporttemplate", "report.rpt"); 
-            //form.Add(new ByteArrayContent(crystalReport), "reporttemplate", "the_dataset_report.rpt");
-            HttpResponseMessage response = await httpClient.PostAsync(serverUrl, form);
+        /// <summary>
+        /// Call a server with a crystal report template and data and have a pdf report returned.
+        /// Is the recommended method for HttpClient performance reasons.
+        /// </summary>
+        /// <param name="reportData"></param>
+        /// <param name="report"></param>
+        /// <param name="httpClient">Manage the HttpClient from the calling program.</param>
+        /// <returns></returns>
+        /// <example>
+        /// <code lang="cs">
+        /// using (var fstream = new FileStream("thereport.rpt", FileMode.Open))
+        /// using (var fstreamOut = new FileStream("thereport.pdf", FileMode.OpenOrCreate | FileMode.Append))
+        /// {
+        ///     var rpt = new Majorsilence.CrystalCmd.Client.Report();
+        ///     using (var stream = await rpt.GenerateAsync(new Data(), fstream, new HttpClient()))
+        ///     {
+        ///         stream.CopyTo(fstreamOut);
+        ///     }
+        /// }
+        ///</code>
+        /// </example>
+        public async Task<Stream> GenerateAsync(Data reportData, Stream report, HttpClient httpClient)
+        {
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(reportData);
 
-            response.EnsureSuccessStatusCode();
-            httpClient.Dispose();
-            //var result = response.Content.ReadAsByteArrayAsync().Result;
-            //string sd = response.Content.ReadAsStringAsync ().Result;
-            //System.IO.File.WriteAllBytes("test_report_from_server.pdf", result);
-
-            return await response.Content.ReadAsStreamAsync();
-
+            using (var form = new MultipartFormDataContent())
+            {
+                form.Add(new StringContent(json), "reportdata");
+                form.Add(new StreamContent(report), "reporttemplate", "report.rpt");
+                //form.Add(new ByteArrayContent(crystalReport), "reporttemplate", "the_dataset_report.rpt");
+                HttpResponseMessage response = await httpClient.PostAsync(serverUrl, form);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStreamAsync();
+            }
         }
 
     }
