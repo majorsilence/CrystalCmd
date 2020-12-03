8bc51a54e44b4da316558accde82bec076b9d99c
Peter Gill <peter@majorsilence.com>
2020-12-03 15:38:44 -0330

    C# client, support both DataTable and Enumerable<T> as default data sources

---
+++ dotnet/DotNetRunner/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetRunner
+{
+    class Employee
+    {
+        public int EMPLOYEE_ID { get; set; }
+        public string LAST_NAME { get; set; }
+        public string FIRST_NAME { get; set; }
+        public DateTime BIRTH_DATE { get; set; }
+    }
+}
