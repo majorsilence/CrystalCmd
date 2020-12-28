782158e459f1c9f5a1e7d4a49687d5ae33d6fb00
Peter Gill <peter@majorsilence.com>
2020-12-27 20:47:42 -0330

    experiment with wonky connections

--- java/CrystalCmd/src/PdfExporter.java
+++ java/CrystalCmd/src/PdfExporter.java
@@ -1,10 +1,18 @@
+import com.businessobjects.prompting.objectmodel.common.IPromptValue;
 import com.crystaldecisions.sdk.occa.report.application.ISubreportClientDocument;
 import com.crystaldecisions.sdk.occa.report.application.OpenReportOptions;
 import com.crystaldecisions.sdk.occa.report.application.ReportClientDocument;
+import com.crystaldecisions.sdk.occa.report.data.Connections;
+import com.crystaldecisions.sdk.occa.report.data.IConnection;
+import com.crystaldecisions.sdk.occa.report.data.IConnectionInfo;
+import com.crystaldecisions.sdk.occa.report.data.Tables;
 import com.crystaldecisions.sdk.occa.report.definition.IReportObject;
 import com.crystaldecisions.sdk.occa.report.exportoptions.ReportExportFormat;
+import com.crystaldecisions.sdk.occa.report.lib.IStrings;
+import com.crystaldecisions.sdk.occa.report.lib.PropertyBag;
 import com.crystaldecisions.sdk.occa.report.lib.ReportSDKException;
 import com.crystaldecisions.sdk.occa.report.application.ParameterFieldController;
+import com.crystaldecisions12.reports.queryengine.collections.ITables;
 
 import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
@@ -66,6 +74,138 @@ public class PdfExporter {
 
         reportClientDocument.open(reportPath, OpenReportOptions._openAsReadOnly);
 
+        /*
+        int connsCount = reportClientDocument.getDatabase().getConnections().size();
+        Connections conns= reportClientDocument.getDatabase().getConnections();
+        reportClientDocument.getDatabase().getConnections().removeAll(conns);
+        //reportClientDocument.getDatabaseController().removeConnection(conns);
+        reportClientDocument.getDatabaseController().getDatabase().getConnections().removeAll(conns);
+
+
+        for (int i=0; i<  conns.size(); i++)
+        {
+            reportClientDocument.getDatabaseController().removeConnection(conns.getConnection(i));
+            conns.remove(i);
+        }
+
+         */
+
+        /*
+        ITable table = reportClientDocument.getDatabaseController().getDatabase().getTables().getTable(0);
+
+        IConnectionInfo connectionInfo = table.getConnectionInfo();
+        PropertyBag propertyBag = connectionInfo.getAttributes();
+        propertyBag.clear();
+
+         */
+
+        //reportClientDocument.getRepositoryLogonInfo().
+
+
+        IStrings subNames = reportClientDocument.getSubreportController().querySubreportNames();
+        for (var name : subNames) {
+            var subReport = reportClientDocument.getSubreportController().getSubreport(name);
+            var subConnInfo = subReport.getDatabaseController().getDatabase().getConnections();
+            for (int i=0;i<subConnInfo.size();  i++)
+            {
+                subReport.getDatabaseController().removeConnection( subConnInfo.getConnection(i));
+
+            }
+
+
+        }
+
+        Connections conns= reportClientDocument.getDatabase().getConnections();
+
+
+
+        for (int i=0; i<conns.size(); i++)
+        {
+            reportClientDocument.getDatabaseController().removeConnection(conns.getConnection(i));
+        }
+
+
+/*
+        var connInfo = reportClientDocument.getDatabaseController().getConnectionInfos(null);
+        for (var x : connInfo) {
+
+            connInfo.forEach(p -> {
+
+
+                PropertyBag propertyBag = p.getAttributes();
+                String name =propertyBag.getStringValue("Server Name");
+                propertyBag.clear();
+
+
+                propertyBag.put("Server Type", "JDBC (JNDI)");
+                propertyBag.put("Server Name", name);
+                propertyBag.put("DriverJarFiles" , "file:/C:/eclipse/plugins/com.businessobjects.crystalreports.samples_12.2.200.r443/SampleDatabases/derby.jar\nfile:/C:/eclipse/plugins/com.businessobjects.crystalreports.samples_12.2.200.r443/SampleDatabases/Xtreme.jar");
+                //"Server Name" -> "jdbc:derby:classpath:Xtreme"
+                propertyBag.put("Database DLL" ,"crdb_jdbc.dll");
+                propertyBag.put("PreQEServerType" , "JDBC (JNDI)");
+                propertyBag.put("Connection URL" , "jdbc:derby:classpath:Xtreme");
+                propertyBag.put("Database Class Name" ,"org.apache.derby.jdbc.EmbeddedDriver");
+                //"Server Type" -> "JDBC (JNDI)"
+                propertyBag.put("PreQEServerName" , "jdbc:derby:classpath:Xtreme");
+                propertyBag.put("JDBC Connection String" ,"!org.apache.derby.jdbc.EmbeddedDriver!jdbc:derby:classpath:Xtreme!user={userid}!password={password}");
+                p.setAttributes(propertyBag);
+            });
+
+            reportClientDocument.refreshReportDocument();
+        }
+        IStrings subNames = reportClientDocument.getSubreportController().querySubreportNames();
+        for (var name : subNames) {
+            var subReport = reportClientDocument.getSubreportController().getSubreport(name);
+            var subConnInfo = subReport.getDatabaseController().getConnectionInfos(null);
+            for (var x : connInfo) {
+
+                subConnInfo.forEach(p -> {
+                    PropertyBag propertyBag = p.getAttributes();
+                    String name2 =propertyBag.getStringValue("Server Name");
+                    propertyBag.clear();
+
+
+                    propertyBag.put("Server Type", "JDBC (JNDI)");
+                    propertyBag.put("Server Name", name2);
+                    propertyBag.put("DriverJarFiles" , "file:/C:/eclipse/plugins/com.businessobjects.crystalreports.samples_12.2.200.r443/SampleDatabases/derby.jar\nfile:/C:/eclipse/plugins/com.businessobjects.crystalreports.samples_12.2.200.r443/SampleDatabases/Xtreme.jar");
+                    //"Server Name" -> "jdbc:derby:classpath:Xtreme"
+                    propertyBag.put("Database DLL" ,"crdb_jdbc.dll");
+                    propertyBag.put("PreQEServerType" , "JDBC (JNDI)");
+                    propertyBag.put("Connection URL" , "jdbc:derby:classpath:Xtreme");
+                    propertyBag.put("Database Class Name" ,"org.apache.derby.jdbc.EmbeddedDriver");
+                    //"Server Type" -> "JDBC (JNDI)"
+                    propertyBag.put("PreQEServerName" , "jdbc:derby:classpath:Xtreme");
+                    propertyBag.put("JDBC Connection String" ,"!org.apache.derby.jdbc.EmbeddedDriver!jdbc:derby:classpath:Xtreme!user={userid}!password={password}");
+
+
+                    p.setAttributes(propertyBag);
+
+                });
+
+                reportClientDocument.refreshReportDocument();
+            }
+        }
+*/
+/*
+        Tables mainTables = reportClientDocument.getDatabaseController().getDatabase().getTables();
+        IStrings subNames = reportClientDocument.getSubreportController().querySubreportNames();
+
+        for (var tab : mainTables) {
+            IConnectionInfo connectionInfo = tab.getConnectionInfo();
+            PropertyBag propertyBag = connectionInfo.getAttributes();
+            propertyBag.clear();
+        }
+
+        for (var subName : subNames
+        ) {
+            Tables subTables = reportClientDocument.getSubreportController().getSubreportDatabase(subName).getTables();
+            for (var tab : subTables) {
+                IConnectionInfo connectionInfo = tab.getConnectionInfo();
+                PropertyBag propertyBag = connectionInfo.getAttributes();
+                propertyBag.clear();
+            }
+        }
+*/
         // Object reportSource = reportClientDocument.getReportSource();
 
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
@@ -111,6 +251,7 @@ public class PdfExporter {
 
                 //try{
                 reportClientDocument.getDatabaseController().setDataSource(result, item.getKey(), item.getKey());
+
                 //	}
                 //	catch(ArrayIndexOutOfBoundsException aibe) {
 
@@ -142,6 +283,7 @@ public class PdfExporter {
                 moveReportObject(reportClientDocument, item);
             }
         }
+
 
         byteArrayInputStream = (ByteArrayInputStream) reportClientDocument.getPrintOutputController()
                 .export(ReportExportFormat.PDF);
