219d8fd31ca4f3f8933867232e1fb8ab628d2d7d
Peter Gill <peter.gill@townsuite.com>
2020-12-30 16:36:19 -0330

    bump netframework server version and include version id in status controller

--- dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.NetFrameworkServer/Controllers/StatusController.cs
+++ dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.NetFrameworkServer/Controllers/StatusController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,10 +10,24 @@ namespace Majorsilence.CrystalCmd.NetFrameworkServer.Controllers
 {
     public class StatusController : ApiController
     {
+        static string _status = null;
+
         // GET api/values
         public string Get()
         {
-            return "Online";
+            if (_status == null)
+            {
+                _status = $"Online - {System.Environment.MachineName} - {GetVersion()}";
+            }
+            return _status;
+        }
+
+        private string GetVersion()
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+            return version;
         }
 
     }
