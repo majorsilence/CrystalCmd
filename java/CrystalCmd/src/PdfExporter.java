91d3103018856c72d8925b314b87dd5d879794d9
Peter Gill <peter.gill@townsuite.com>
2020-12-30 12:20:21 -0330

    add default parameters in java implementation

--- java/CrystalCmd/src/PdfExporter.java
+++ java/CrystalCmd/src/PdfExporter.java
@@ -2,10 +2,7 @@ import com.businessobjects.prompting.objectmodel.common.IPromptValue;
 import com.crystaldecisions.sdk.occa.report.application.ISubreportClientDocument;
 import com.crystaldecisions.sdk.occa.report.application.OpenReportOptions;
 import com.crystaldecisions.sdk.occa.report.application.ReportClientDocument;
-import com.crystaldecisions.sdk.occa.report.data.Connections;
-import com.crystaldecisions.sdk.occa.report.data.IConnection;
-import com.crystaldecisions.sdk.occa.report.data.IConnectionInfo;
-import com.crystaldecisions.sdk.occa.report.data.Tables;
+import com.crystaldecisions.sdk.occa.report.data.*;
 import com.crystaldecisions.sdk.occa.report.definition.IReportObject;
 import com.crystaldecisions.sdk.occa.report.exportoptions.ReportExportFormat;
 import com.crystaldecisions.sdk.occa.report.lib.IStrings;
@@ -62,150 +59,32 @@ public class PdfExporter {
         ByteArrayInputStream byteArrayInputStream;
 
 
-        /*
-         * Instantiate ReportClientDocument and specify the Java Print Engine as the
-         * report processor. Open a rpt file and export to PDF. Stream PDF back to web
-         * browser.
-         */
-
         reportClientDocument = new ReportClientDocument();
 
         reportClientDocument.setReportAppServer(ReportClientDocument.inprocConnectionString);
 
         reportClientDocument.open(reportPath, OpenReportOptions._openAsReadOnly);
 
-        /*
-        int connsCount = reportClientDocument.getDatabase().getConnections().size();
-        Connections conns= reportClientDocument.getDatabase().getConnections();
-        reportClientDocument.getDatabase().getConnections().removeAll(conns);
-        //reportClientDocument.getDatabaseController().removeConnection(conns);
-        reportClientDocument.getDatabaseController().getDatabase().getConnections().removeAll(conns);
-
-
-        for (int i=0; i<  conns.size(); i++)
-        {
-            reportClientDocument.getDatabaseController().removeConnection(conns.getConnection(i));
-            conns.remove(i);
-        }
-
-         */
-
-        /*
-        ITable table = reportClientDocument.getDatabaseController().getDatabase().getTables().getTable(0);
-
-        IConnectionInfo connectionInfo = table.getConnectionInfo();
-        PropertyBag propertyBag = connectionInfo.getAttributes();
-        propertyBag.clear();
-
-         */
-
-        //reportClientDocument.getRepositoryLogonInfo().
-
 
         IStrings subNames = reportClientDocument.getSubreportController().querySubreportNames();
         for (var name : subNames) {
             var subReport = reportClientDocument.getSubreportController().getSubreport(name);
             var subConnInfo = subReport.getDatabaseController().getDatabase().getConnections();
-            for (int i=0;i<subConnInfo.size();  i++)
-            {
-                subReport.getDatabaseController().removeConnection( subConnInfo.getConnection(i));
+            for (int i = 0; i < subConnInfo.size(); i++) {
+                subReport.getDatabaseController().removeConnection(subConnInfo.getConnection(i));
 
             }
 
-
         }
 
-        Connections conns= reportClientDocument.getDatabase().getConnections();
+        Connections conns = reportClientDocument.getDatabase().getConnections();
 
 
-
-        for (int i=0; i<conns.size(); i++)
-        {
+        for (int i = 0; i < conns.size(); i++) {
             reportClientDocument.getDatabaseController().removeConnection(conns.getConnection(i));
         }
 
 
-/*
-        var connInfo = reportClientDocument.getDatabaseController().getConnectionInfos(null);
-        for (var x : connInfo) {
-
-            connInfo.forEach(p -> {
-
-
-                PropertyBag propertyBag = p.getAttributes();
-                String name =propertyBag.getStringValue("Server Name");
-                propertyBag.clear();
-
-
-                propertyBag.put("Server Type", "JDBC (JNDI)");
-                propertyBag.put("Server Name", name);
-                propertyBag.put("DriverJarFiles" , "file:/C:/eclipse/plugins/com.businessobjects.crystalreports.samples_12.2.200.r443/SampleDatabases/derby.jar\nfile:/C:/eclipse/plugins/com.businessobjects.crystalreports.samples_12.2.200.r443/SampleDatabases/Xtreme.jar");
-                //"Server Name" -> "jdbc:derby:classpath:Xtreme"
-                propertyBag.put("Database DLL" ,"crdb_jdbc.dll");
-                propertyBag.put("PreQEServerType" , "JDBC (JNDI)");
-                propertyBag.put("Connection URL" , "jdbc:derby:classpath:Xtreme");
-                propertyBag.put("Database Class Name" ,"org.apache.derby.jdbc.EmbeddedDriver");
-                //"Server Type" -> "JDBC (JNDI)"
-                propertyBag.put("PreQEServerName" , "jdbc:derby:classpath:Xtreme");
-                propertyBag.put("JDBC Connection String" ,"!org.apache.derby.jdbc.EmbeddedDriver!jdbc:derby:classpath:Xtreme!user={userid}!password={password}");
-                p.setAttributes(propertyBag);
-            });
-
-            reportClientDocument.refreshReportDocument();
-        }
-        IStrings subNames = reportClientDocument.getSubreportController().querySubreportNames();
-        for (var name : subNames) {
-            var subReport = reportClientDocument.getSubreportController().getSubreport(name);
-            var subConnInfo = subReport.getDatabaseController().getConnectionInfos(null);
-            for (var x : connInfo) {
-
-                subConnInfo.forEach(p -> {
-                    PropertyBag propertyBag = p.getAttributes();
-                    String name2 =propertyBag.getStringValue("Server Name");
-                    propertyBag.clear();
-
-
-                    propertyBag.put("Server Type", "JDBC (JNDI)");
-                    propertyBag.put("Server Name", name2);
-                    propertyBag.put("DriverJarFiles" , "file:/C:/eclipse/plugins/com.businessobjects.crystalreports.samples_12.2.200.r443/SampleDatabases/derby.jar\nfile:/C:/eclipse/plugins/com.businessobjects.crystalreports.samples_12.2.200.r443/SampleDatabases/Xtreme.jar");
-                    //"Server Name" -> "jdbc:derby:classpath:Xtreme"
-                    propertyBag.put("Database DLL" ,"crdb_jdbc.dll");
-                    propertyBag.put("PreQEServerType" , "JDBC (JNDI)");
-                    propertyBag.put("Connection URL" , "jdbc:derby:classpath:Xtreme");
-                    propertyBag.put("Database Class Name" ,"org.apache.derby.jdbc.EmbeddedDriver");
-                    //"Server Type" -> "JDBC (JNDI)"
-                    propertyBag.put("PreQEServerName" , "jdbc:derby:classpath:Xtreme");
-                    propertyBag.put("JDBC Connection String" ,"!org.apache.derby.jdbc.EmbeddedDriver!jdbc:derby:classpath:Xtreme!user={userid}!password={password}");
-
-
-                    p.setAttributes(propertyBag);
-
-                });
-
-                reportClientDocument.refreshReportDocument();
-            }
-        }
-*/
-/*
-        Tables mainTables = reportClientDocument.getDatabaseController().getDatabase().getTables();
-        IStrings subNames = reportClientDocument.getSubreportController().querySubreportNames();
-
-        for (var tab : mainTables) {
-            IConnectionInfo connectionInfo = tab.getConnectionInfo();
-            PropertyBag propertyBag = connectionInfo.getAttributes();
-            propertyBag.clear();
-        }
-
-        for (var subName : subNames
-        ) {
-            Tables subTables = reportClientDocument.getSubreportController().getSubreportDatabase(subName).getTables();
-            for (var tab : subTables) {
-                IConnectionInfo connectionInfo = tab.getConnectionInfo();
-                PropertyBag propertyBag = connectionInfo.getAttributes();
-                propertyBag.clear();
-            }
-        }
-*/
         // Object reportSource = reportClientDocument.getReportSource();
 
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
@@ -246,43 +125,48 @@ public class PdfExporter {
                     }
                 }
             }
-            for (Map.Entry<String, Object> item : datafile.getParameters().entrySet()) {
-                ParameterFieldController parameterFieldController;
 
+            var z = reportClientDocument.getDataDefController().getDataDefinition().getParameterFields();
+            for(var blah : z){
+                if(blah.getParameterType() == ParameterFieldType.reportParameter){
+                    System.out.println("hi " + blah.getName() + " " + blah.getType().toVariantTypeString()+ " " + blah.getType());
 
-                parameterFieldController = reportClientDocument.getDataDefController().getParameterFieldController();
-
-                Date value = null;
-                try {
-                    value = fmt.parse(item.getValue().toString());
-                } catch (ParseException e) {
-                }
-
-                try {
-                    if (value == null) {
-                        parameterFieldController.setCurrentValue("", item.getKey(), item.getValue());
-                    } else {
-                        parameterFieldController.setCurrentValue("", item.getKey(), value);
+                    String theValue;
+                    switch(blah.getType().toVariantTypeString().toLowerCase()){
+                        case "string":
+                            SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), ""));
+                            break;
+                        case "boolean":
+                        case "i1":
+                            SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), false));
+                            break;
+                        case "number":
+                        case "decimal":
+                            SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), 0));
+                            break;
+                        case "date":
+                        case "datetime":
+                            SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), new Date()));
+                            break;
+                        default:
+                            SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), ""));
+                            break;
                     }
-                } catch (com.crystaldecisions.sdk.occa.report.lib.ReportSDKInvalidParameterFieldCurrentValueException notfoundParameter) {
-                    // doesn't matter, but should add logging
+
                 }
-                /*
-                 * parameterFieldController.setCurrentValue("", "StringParam", "Hello");
-                 * parameterFieldController.setCurrentValue("sub", "StringParam",
-                 * "Subreport string value"); parameterFieldController.setCurrentValue("",
-                 * "BooleanParam", new Boolean(true));
-                 * parameterFieldController.setCurrentValue("", "CurrencyParam", new
-                 * Double(123.45)); parameterFieldController.setCurrentValue("", "NumberParam",
-                 * new Integer(123));
-                 *
-                 */
-                // rpt.SetParameterValue(item.Key, item.Value);
+
+            }
+
+            for (Map.Entry<String, Object> item : datafile.getParameters().entrySet()) {
+                SetParameterValue(reportClientDocument, fmt, item);
             }
             for (Iterator<MoveObjects> itr = datafile.getMoveObjectPosition().iterator(); itr.hasNext(); ) {
                 MoveObjects item = itr.next();
                 moveReportObject(reportClientDocument, item);
             }
+
+
+
         }
 
 
@@ -291,6 +175,42 @@ public class PdfExporter {
 
         reportClientDocument.close();
         return byteArrayInputStream;
+    }
+
+    private void SetParameterValue(ReportClientDocument reportClientDocument, SimpleDateFormat fmt, Map.Entry<String, Object> item) throws ReportSDKException {
+        ParameterFieldController parameterFieldController;
+
+
+        parameterFieldController = reportClientDocument.getDataDefController().getParameterFieldController();
+
+
+        Date value = null;
+        try {
+            value = fmt.parse(item.getValue().toString());
+        } catch (ParseException e) {
+        }
+
+
+        try {
+            if (value == null) {
+                parameterFieldController.setCurrentValue("", item.getKey(), item.getValue());
+            } else {
+                parameterFieldController.setCurrentValue("", item.getKey(), value);
+            }
+        } catch (com.crystaldecisions.sdk.occa.report.lib.ReportSDKInvalidParameterFieldCurrentValueException notfoundParameter) {
+            // doesn't matter, but should add logging
+        }
+        /*
+         * parameterFieldController.setCurrentValue("", "StringParam", "Hello");
+         * parameterFieldController.setCurrentValue("sub", "StringParam",
+         * "Subreport string value"); parameterFieldController.setCurrentValue("",
+         * "BooleanParam", new Boolean(true));
+         * parameterFieldController.setCurrentValue("", "CurrencyParam", new
+         * Double(123.45)); parameterFieldController.setCurrentValue("", "NumberParam",
+         * new Integer(123));
+         *
+         */
+        // rpt.SetParameterValue(item.Key, item.Value);
     }
 
     private void moveReportObject(ReportClientDocument reportClientDocument, MoveObjects item)
