f1d0e5f30ce105733088d09476fc50ccdf5b8a84
Peter Gill <peter.gill@townsuite.com>
2021-03-03 11:53:36 -0330

    remove unused bits

--- java/CrystalCmd/src/ReportType.java
+++
@@ -1,5 +0,0 @@
-    public enum ReportType {       
-        PDF,
-        ZIP,      
-        NONE,
-    }
--- java/CrystalCmd/src/SubReports.java
+++
@@ -1,9 +0,0 @@
-
-public class SubReports {
-    public String ReportName;
-    
-    public String TableName;
-    
-    public String DataTable;
-    
-}
