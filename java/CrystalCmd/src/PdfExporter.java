163da5a21c6cf6e65f439d6ebe2b19e63e97909c
Peter Gill <peter.gill@townsuite.com>
2021-12-20 20:22:05 -0330

    Improvements to java server implementation.
    Add java server Readme.md
    
    Note the issue with fonts.  If fonts are not in the correct location
    the OpenJDK installs will not function/export pdf documents.

--- java/CrystalCmd/src/PdfExporter.java
+++ java/CrystalCmd/src/PdfExporter.java
@@ -20,8 +20,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.Locale;
 import java.util.Map;
 
 public class PdfExporter {
@@ -58,26 +64,22 @@ public class PdfExporter {
 
 
         reportClientDocument = new ReportClientDocument();
-
         reportClientDocument.setReportAppServer(ReportClientDocument.inprocConnectionString);
-
         reportClientDocument.open(reportPath, OpenReportOptions._openAsReadOnly);
 
-
+        // Remove sub report connection strings
         IStrings subNames = reportClientDocument.getSubreportController().querySubreportNames();
         for (var name : subNames) {
             var subReport = reportClientDocument.getSubreportController().getSubreport(name);
             var subConnInfo = subReport.getDatabaseController().getDatabase().getConnections();
             for (int i = 0; i < subConnInfo.size(); i++) {
-                subReport.getDatabaseController().removeConnection(subConnInfo.getConnection(i));
+                // subReport.getDatabaseController().removeConnection(subConnInfo.getConnection(i));
 
             }
-
         }
 
+        // Remove main report connection strings
         Connections conns = reportClientDocument.getDatabase().getConnections();
-
-
         for (int i = 0; i < conns.size(); i++) {
             reportClientDocument.getDatabaseController().removeConnection(conns.getConnection(i));
         }
@@ -88,6 +90,7 @@ public class PdfExporter {
         // "06/24/2020 00:00:00"
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
+        // If not data just abort and return early
         if (datafile == null) {
             byteArrayInputStream = (ByteArrayInputStream) reportClientDocument.getPrintOutputController()
                     .export(ReportExportFormat.RTF);
@@ -96,8 +99,8 @@ public class PdfExporter {
             return byteArrayInputStream;
         }
 
+        // Set Main Report Result Set
         for (Map.Entry<String, String> item : datafile.getDataTables().entrySet()) {
-
             CsharpResultSet inst = new CsharpResultSet();
             ResultSet result = inst.Execute(item.getValue());
 
@@ -111,28 +114,25 @@ public class PdfExporter {
             for (Iterator<SubReports> itr = datafile.getSubReportDataTables().iterator(); itr.hasNext(); ) {
                 SubReports item = itr.next();
                 try {
-
-
                     CsharpResultSet inst = new CsharpResultSet();
                     ResultSet result = inst.Execute(item.DataTable);
                     String subReportName = item.ReportName;
                     String tableName = item.TableName;
 
-                    //set resultSet for sub report
+                    // Set Sub Report ResultSet
                     ISubreportClientDocument subClientDoc = reportClientDocument.getSubreportController().getSubreport(subReportName);
                     subClientDoc.getDatabaseController().setDataSource(result, tableName, tableName);
-                   // inst.close();
-                }
-                catch(ReportSDKException rse){
+                    // inst.close();
+                } catch (ReportSDKException rse) {
                     rse.printStackTrace();
-                }
-                catch (ArrayIndexOutOfBoundsException aiofbe) {
+                } catch (ArrayIndexOutOfBoundsException aiofbe) {
                     // add logging
                     aiofbe.printStackTrace();
                 }
             }
         }
 
+        /*
         var z = reportClientDocument.getDataDefController().getDataDefinition().getParameterFields();
         for (var blah : z) {
             if (blah.getParameterType() == ParameterFieldType.reportParameter) {
@@ -163,6 +163,7 @@ public class PdfExporter {
             }
 
         }
+*/
 
         for (Map.Entry<String, Object> item : datafile.getParameters().entrySet()) {
             SetParameterValue(reportClientDocument, fmt, item);
@@ -171,8 +172,6 @@ public class PdfExporter {
             MoveObjects item = itr.next();
             moveReportObject(reportClientDocument, item);
         }
-
-
 
 
         byteArrayInputStream = (ByteArrayInputStream) reportClientDocument.getPrintOutputController()
@@ -188,6 +187,158 @@ public class PdfExporter {
 
 
         parameterFieldController = reportClientDocument.getDataDefController().getParameterFieldController();
+
+        String name = item.getKey();
+        var parameters = reportClientDocument.getDataDefController().getDataDefinition().getParameterFields();
+        for (var field : parameters) {
+
+            try {
+                if (field.getParameterType() != ParameterFieldType.reportParameter) {
+                    continue;
+                }
+                if (field.getName().toLowerCase().equals(name.toLowerCase()) == false) {
+                    continue;
+                }
+
+                System.out.println("hi " + field.getName() + " " + field.getType().toVariantTypeString() + " " + field.getType());
+                System.out.println(name);
+
+                field.getType();
+
+                Object val = item.getValue();
+                String theValue = "";
+                switch (field.getType().toVariantTypeString().toLowerCase()) {
+                    case "string":
+                        theValue = val.toString();
+                        if(isEmptyString(theValue)){
+                            theValue = "";
+                        }
+                        parameterFieldController.setCurrentValue("", name, theValue);
+                        break;
+                    case "boolean":
+                    case "i1":
+                        if (isEmptyString(val.toString())) {
+                            theValue = "false";
+                        } else {
+                            theValue = val.toString();
+                        }
+                        if (theValue == "0") {
+                            theValue = "false";
+                        } else if (theValue == "1") {
+                            theValue = "true";
+                        }
+
+                        boolean toSetValue =  Boolean.parseBoolean(theValue);
+                        parameterFieldController.setCurrentValue("", name, toSetValue);
+                        break;
+                    case "number":
+                    case "decimal":
+                        if (isEmptyString(val.toString())) {
+                            theValue = "0";
+                        } else {
+                            theValue = val.toString();
+                        }
+
+                        try {
+                            int toSetValueInt = Integer.parseInt(theValue);
+                            parameterFieldController.setCurrentValue("", name, toSetValueInt);
+                        } catch (Exception e) {
+                            // TODO: should this be a BigDecimal?
+                            double toSetDouble =  Double.parseDouble(theValue);
+                            parameterFieldController.setCurrentValue("", name, toSetDouble);
+                        }
+                        break;
+                    case "date":
+                    case "datetime":
+                        Date dateValue;
+                        if (isEmptyString(val.toString())){
+                            dateValue = new Date();
+                        }
+                        else{
+                           /*
+                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(""
+                                    + "[yyyy-MM-dd'T'HH:mm:ss]"
+                                    + "[yyyy-MM-dd'T'HH:mm:ss.SSSXXX]"
+                                    + "[MM/dd/yyyy HH:mm:ss]"
+                                    + "[yyyy/MM/dd HH:mm:ss.SSSSSS]"
+                                    + "[yyyy-MM-dd HH:mm:ss[.SSS]]"
+                                    + "[ddMMMyyyy:HH:mm:ss.SSS[ Z]]"
+                            );
+                            */
+                            DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                                    .append(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+                                    .toFormatter();
+                            dateValue = Date.from(LocalDateTime.parse(val.toString(), formatter).toInstant(ZoneOffset.ofHours(0)));
+                        }
+
+                        //parameterFieldController.setCurrentValue("", name, Date..parseDouble(theValue));
+                        parameterFieldController.setCurrentValue("", name, dateValue);
+
+                        break;
+                    default:
+                        theValue = val.toString();
+                        if(isEmptyString(theValue)){
+                            theValue = "";
+                        }
+                        parameterFieldController.setCurrentValue("", name, theValue);
+                        break;
+                }
+
+
+/*
+                String theValue;
+                switch (par.ParameterValueType) {
+                    case ParameterValueKind.CurrencyParameter:
+                        theValue = string.IsNullOrWhiteSpace(val ?.ToString()) ?"0" :val.ToString();
+                        rpt.SetParameterValue(name, decimal.Parse(theValue));
+                        break;
+                    case ParameterValueKind.NumberParameter:
+
+
+                        break;
+                    case ParameterValueKind.DateParameter:
+                    case ParameterValueKind.DateTimeParameter:
+                    case ParameterValueKind.TimeParameter:
+
+                        break;
+                    default:
+                        theValue = string.IsNullOrWhiteSpace(val ?.ToString()) ?" " :val.ToString();
+                        rpt.SetParameterValue(name, theValue);
+                        break;
+                }
+                */
+            } catch (Exception ex) {
+                System.out.println("Parameter " + name + " failure");
+                ex.printStackTrace();
+            }
+
+
+        }
+
+
+/*
+        String theValue;
+        switch (blah.getType().toVariantTypeString().toLowerCase()) {
+            case "string":
+                SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), ""));
+                break;
+            case "boolean":
+            case "i1":
+                SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), false));
+                break;
+            case "number":
+            case "decimal":
+                SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), 0));
+                break;
+            case "date":
+            case "datetime":
+                SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), new Date()));
+                break;
+            default:
+                SetParameterValue(reportClientDocument, fmt, Map.entry(blah.getName(), ""));
+                break;
+        }
+
 
 
         Date value = null;
@@ -206,6 +357,8 @@ public class PdfExporter {
         } catch (com.crystaldecisions.sdk.occa.report.lib.ReportSDKInvalidParameterFieldCurrentValueException notfoundParameter) {
             // doesn't matter, but should add logging
         }
+        */
+
         /*
          * parameterFieldController.setCurrentValue("", "StringParam", "Hello");
          * parameterFieldController.setCurrentValue("sub", "StringParam",
@@ -217,6 +370,10 @@ public class PdfExporter {
          *
          */
         // rpt.SetParameterValue(item.Key, item.Value);
+    }
+
+    boolean isEmptyString(String string) {
+        return string == null || string.isEmpty();
     }
 
     private void moveReportObject(ReportClientDocument reportClientDocument, MoveObjects item)
--- java/Readme.md
+++ java/Readme.md
@@ -11,6 +11,30 @@ https://wiki.scn.sap.com/wiki/display/BOBJ/Crystal+Reports+Java++SDK+Samples#Cry
 https://origin.softwaredownloads.sap.com/public/site/index.html
 
 
+## Crystal Reports and PDF Export ** IMPORTANT - WILL NOT WORK WITHOUT THIS STEP**
+
+### Linux requires fonts installed
+```bash
+# try the ubuntu or fedora way first
+# https://answers.sap.com/questions/676449/nullpointerexception-in-opentypefontmanager.html
+apk add --no-cache msttcorefonts-installer && update-ms-fonts && fc-cache -f && ln -s /usr/share/fonts/truetype/msttcorefonts /usr/lib/jvm/default-jvm/jre/lib/fonts
+```
+
+```bash
+# ubuntu
+sudo apt install fonts-dejavu fontconfig 
+
+# fedora
+dnf install fontconfig dejavu-sans-fonts dejavu-serif-fonts
+```
+
+### Windows 
+
+copy C:\Windows\fonts into the jre/jdk lib/fonts folder.
+
+For example copy 'C:\Windows\fonts' into 'C:\Users\[UserName]\.jdks\openjdk-17.0.1\lib\fonts'
+
+
 ## CSV ResultSet Driver
 https://sourceforge.net/projects/csvjdbc/
 
