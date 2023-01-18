becfc0bb7a4261813bad3b8eed6e7ee87a8c3dfa
Peter Gill <peter.gill@townsuite.com>
2023-01-18 13:23:24 -0330

    Switch to Mono.Net.HttpListener

--- dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.NetframeworkConsoleServer/WebServer.cs
+++ dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.NetframeworkConsoleServer/WebServer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
-using System.Net;
+using Mono.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
