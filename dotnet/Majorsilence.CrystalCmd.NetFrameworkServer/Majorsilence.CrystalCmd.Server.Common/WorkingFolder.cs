b256c66040fff8c0b2ee5cfc0810a704b0e348d2
Peter Gill <peter.gill@townsuite.com>
2023-02-16 20:27:34 -0330

    Use a custom subdirectory inside of temp
    On startup ensure a clean working directory.

---
+++ dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.Server.Common/WorkingFolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majorsilence.CrystalCmd.Server.Common
+{
+    public static class WorkingFolder
+    {
+        public static string GetMajorsilenceTempFolder()
+        {
+            return Path.Combine(Path.GetTempPath(), "majorsilence", "crystalcmd");
+        }
+    }
+}
