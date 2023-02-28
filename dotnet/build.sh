bf2be7d2d015768f4b7dc5c0de2a62e208e26921
Peter Gill <peter.gill@townsuite.com>
2023-02-28 11:05:04 -0330

    Create build.sh

---
+++ dotnet/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e # exit on first error
+set -u # exit on using unset variable
+
+cwd=$(pwd)
+
+echo "build base image"
+docker build --progress plain -f Dockerfile -t majorsilence/dotnet_framework_wine -m 4GB .
+
+echo "build crystal reports service image"
+docker build --progress plain --no-cache --build-arg A_CRYSTALCMD_VERSION="1.0.13.0" -f Dockerfile -t majorsilence/dotnet_framework_wine_crystalcmd -m 4GB .
