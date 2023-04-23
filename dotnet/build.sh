73c11be3bc9f11f9f43513782dd284e9b9f23b78
Peter Gill <peter@majorsilence.com>
2023-04-23 12:46:47 -0230

    Update build.sh
    make executable and fix dockerfile path

--- dotnet/build.sh
+++ dotnet/build.sh
@@ -5,7 +5,7 @@ set -u # exit on using unset variable
 cwd=$(pwd)
 
 echo "build base image"
-docker build --progress plain -f Dockerfile -t majorsilence/dotnet_framework_wine -m 4GB .
+docker build --progress plain -f Dockerfile.wine -t majorsilence/dotnet_framework_wine -m 4GB .
 
 echo "build crystal reports service image"
-docker build --progress plain --no-cache --build-arg A_CRYSTALCMD_VERSION="1.0.13.0" -f Dockerfile -t majorsilence/dotnet_framework_wine_crystalcmd -m 4GB .
+docker build --progress plain --no-cache --build-arg A_CRYSTALCMD_VERSION="1.0.13.0" -f Dockerfile.crystalcmd -t majorsilence/dotnet_framework_wine_crystalcmd -m 4GB .
