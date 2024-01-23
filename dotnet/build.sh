84b57c9998a9c31cc22e7c4f7387f13308686bcf
Peter Gill <peter@majorsilence.com>
2024-01-23 19:45:06 -0330

    Update build.sh

--- dotnet/build.sh
+++ dotnet/build.sh
@@ -3,19 +3,19 @@ set -e # exit on first error
 set -u # exit on using unset variable
 
 cwd=$(pwd)
-
+DOCKER_BUILDKIT=1
 version=`cat VERSION_WINE`
 echo "version: $version"
 
 echo "build base image"
-docker build --progress plain -f Dockerfile.wine -t majorsilence/dotnet_framework_wine -m 4GB .
+docker build -f Dockerfile.wine -t majorsilence/dotnet_framework_wine -m 4GB .
 docker tag majorsilence/dotnet_framework_wine:latest majorsilence/dotnet_framework_wine:$version
 #docker push majorsilence/dotnet_framework_wine:latest
 #docker push majorsilence/dotnet_framework_wine:$version
 
 crystal_cmd_version=`cat VERSION_CRYSTALCMD`
 echo "build crystal reports service image"
-docker build --progress plain --no-cache --build-arg A_CRYSTALCMD_VERSION="$crystal_cmd_version" -f Dockerfile.crystalcmd -t majorsilence/dotnet_framework_wine_crystalcmd -m 4GB .
+docker build --no-cache --build-arg A_CRYSTALCMD_VERSION="$crystal_cmd_version" -f Dockerfile.crystalcmd -t majorsilence/dotnet_framework_wine_crystalcmd -m 4GB .
 docker tag majorsilence/dotnet_framework_wine_crystalcmd:latest majorsilence/dotnet_framework_wine_crystalcmd:$crystal_cmd_version
 #docker push majorsilence/dotnet_framework_wine_crystalcmd:latest
 #docker push majorsilence/dotnet_framework_wine_crystalcmd:$crystal_cmd_version
