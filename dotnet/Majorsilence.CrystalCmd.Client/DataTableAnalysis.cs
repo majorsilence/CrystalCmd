794f9abb757db531908cf3dbec9409ba90cf0fdf
Peter Gill <peter.gill@townsuite.com>
2024-02-29 15:18:30 -0330

    Crystal Reports Analyzer
    bits and pieces taken from https://github.com/majorsilence/CrystalCmd/pull/34

---
+++ dotnet/Majorsilence.CrystalCmd.Client/DataTableAnalysis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Majorsilence.CrystalCmd.Client
+{
+    public class DataTableAnalysis
+    {
+        internal DataTableAnalysis(string dataTableName, IEnumerable<string> columnNames)
+        {
+            DataTableName = dataTableName;
+            ColumnNames = columnNames;
+        }
+
+        public string DataTableName { get; }
+        public IEnumerable<string> ColumnNames { get; }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            var seperatorCount = 15;
+            var seperator = new string('-', seperatorCount);
+            builder.AppendLine("Data Table: " + DataTableName);
+            builder.AppendLine(seperator);
+            builder.AppendLine("Columns: " + string.Join(", ", ColumnNames));
+            builder.AppendLine(seperator);
+            return builder.ToString();
+        }
+    }
+}
---
+++ dotnet/Majorsilence.CrystalCmd.Client/ReportAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+
+namespace Majorsilence.CrystalCmd.Client
+{
+    public class ReportAnalyzer
+    {
+        HttpClient _client;
+        Stream _report;
+        string _serverUrl;
+        string _username;
+        string _password;
+        string _userAgent;
+        public ReportAnalyzer(Stream report, HttpClient client,
+            string username, string password, string serverUrl = "https://c.majorsilence.com/analyzer",
+            string userAgent = "Majorsilence.CrystalCmd.Client/1.0.0 Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0")
+        {
+            _report = report;
+            _client = client;
+            _serverUrl = serverUrl;
+            _username = username;
+            _password = password;
+            _userAgent = userAgent;
+        }
+
+        public async Task<FullReportAnalysis> FullAnalysis(CancellationToken cancellationToken = default)
+        {
+            Func<FullReportAnalysisResponse.DataTableAnalysisDto, DataTableAnalysis> createDataTable = t => new DataTableAnalysis(t.DataTableName, t.ColumnNames);
+            var dto = await ServerRequest<object, FullReportAnalysisResponse>(null, "/FullAnalysis", _report, cancellationToken);
+            return new FullReportAnalysis(
+                dto.Parameters,
+                dto.DataTables.Select(createDataTable),
+                dto.SubReports.Select(s => new FullSubReportAnalysis(s.SubreportName, s.Parameters, s.DataTables.Select(createDataTable)))
+            );
+        }
+
+        private async Task<U> ServerRequest<T, U>(T reportRequest, string analyzerEndPoint, Stream report, CancellationToken cancellationToken)
+        {
+            var serverCaller = new ServerCaller(_client, _serverUrl);
+            using (var response = await serverCaller.GenerateAsync(reportRequest, report, analyzerEndPoint,
+                _username, _password, _userAgent, cancellationToken))
+            {
+                using (var responseReader = new StreamReader(response))
+                {
+                    var responseString = await responseReader.ReadToEndAsync();
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<U>(responseString);
+                }
+            }
+        }
+    }
+}
