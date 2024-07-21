3d0c1ae10bc779941a685f996c0c0cb3f47bda14
Peter Gill <peter@majorsilence.com>
2024-07-21 14:30:30 -0230

    Update startup.sh
    wget, stop writing a file per internal bash health check

--- dotnet/startup.sh
+++ dotnet/startup.sh
@@ -18,7 +18,7 @@ run_x64_service
 echo "Running $WINEARCH"
 
 while true; do
-    responsecode=$(wget --tries=1 --timeout=5 --server-response http://127.0.0.1:44355/healthz/ready 2>&1 | awk '/^  HTTP/{print $2}')
+    responsecode=$(wget --tries=1 --timeout=5 --server-response -O /dev/null http://127.0.0.1:44355/healthz/ready 2>&1 | awk '/^  HTTP/{print $2}')
     if [ "$responsecode" != "200" ] ; then
         echo "Health check failed with response code $responsecode"
         wget --tries=1 --timeout=5 --server-response http://127.0.0.1:44355/healthz/ready
