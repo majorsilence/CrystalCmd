48f9aa9ec318ccf558e03aca53098f71e9874f94
Peter Gill <peter@majorsilence.com>
2024-07-26 20:20:26 -0230

    nuget central package management

---
+++ dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.Client/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Majorsilence.CrystalCmd.Client
+{
+    [Obsolete("This class is obsolete, use Majorsilence.CrystalCmd.Common.Data instead.")]
+    public class Data : Common.Data
+    {
+       
+    }
+}
