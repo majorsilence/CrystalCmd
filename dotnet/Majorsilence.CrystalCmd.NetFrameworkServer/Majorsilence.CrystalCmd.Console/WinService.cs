7f58c4c7b423cd5104566e12a9fd1861d2bfa79e
Peter Gill <peter.gill@townsuite.com>
2025-11-17 13:31:55 -0330

    Refactor queue processing and add Windows Service support
    Refactored the application to improve modularity and maintainability:
    - Added `ExportQueue` class for background queue processing.
    - Introduced `WinService` class to enable running as a Windows Service.
    - Refactored `Program.cs` to delegate queue logic to `ExportQueue`.
    - Added support for Crystal Reports assemblies for report generation.
    - Improved logging, error handling, and code readability.

---
+++ dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.Console/WinService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.ServiceProcess;
+
+namespace Majorsilence.CrystalCmd.NetframeworkConsole
+{
+    internal class WinService : ServiceBase
+    {
+        private readonly string _serviceName;
+        private readonly ILogger _logger;
+        ExportQueue export;
+        public WinService(string serviceName, ILogger logger)
+        {
+            _serviceName = serviceName;
+            ServiceName = serviceName;
+            _logger = logger;
+        }
+        protected override void OnStart(string[] args)
+        {
+            // Start your service logic here
+            export = new ExportQueue(_logger);
+            export.Start();
+            Console.WriteLine($"{_serviceName} started.");
+        }
+        protected override void OnStop()
+        {
+            // Clean up your service logic here
+            export.Stop();
+            Console.WriteLine($"{_serviceName} stopped.");
+        }
+    }
+}
