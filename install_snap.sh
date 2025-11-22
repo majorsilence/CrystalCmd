3cd83c3c58767b8177ca2bed1e19652b73f51d92
Peter Gill <peter@majorsilence.com>
2025-11-22 15:18:47 -0330

    remove snapcraft/snap support

--- install_snap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e # exit on first error
-set -u # exit on using unset variable
-
-
-snap install ./java/CrystalCmd/build/CrystalCmd.snap --force-dangerous --classic # force dangerous is needed because the snap is not signed
-
-scp ./java/CrystalCmd/build/CrystalCmd.snap root@c.majorsilence.com:/root
-ssh root@c.majorsilence.com 'snap install /root/CrystalCmd.snap --force-dangerous --classic'
