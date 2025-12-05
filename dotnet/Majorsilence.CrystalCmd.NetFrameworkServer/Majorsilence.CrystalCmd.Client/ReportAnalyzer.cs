bec0309ce9e59f3c7d90ef36167113d0d5f490af
Peter Gill <peter.gill@townsuite.com>
2025-12-05 09:51:39 -0330

    Report analysis client and server; add polling support and refactor HTTP request handling
    Refactored `Task.Delay` to support cancellation tokens.
    Updated `ReportAnalyzer` to include polling functionality for long-running operations via the new `PollGet` method.
    
    Introduced `GenerateAsync` to centralize HTTP request handling, supporting both polling and non-polling modes. Removed the `ServerCaller` class and integrated its functionality into `ReportAnalyzer`.
    
    Enhanced error handling with detailed messages and added the `Base64Encode` helper method for authorization headers.
    
    Added unit test `Test_Analyzer_Polling` to verify polling functionality. Improved test coverage, code organization, and maintainability. Simplified the API with overloads for `FullAnalysis` and `Analyze` methods.

--- dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.Client/ReportAnalyzer.cs
+++ dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.Client/ReportAnalyzer.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Majorsilence.CrystalCmd.Common;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
-using System.Linq;
+using System.Threading.Tasks;
 
 namespace Majorsilence.CrystalCmd.Client
 {
@@ -49,8 +51,18 @@ namespace Majorsilence.CrystalCmd.Client
 
         public async Task<FullReportAnalysis> FullAnalysis(CancellationToken cancellationToken = default)
         {
+            return await FullAnalysis(cancellationToken, false);
+        }
+
+        public async Task<Common.FullReportAnalysisResponse> Analyze(CancellationToken cancellationToken = default)
+        {
+            return await Analyze(cancellationToken, false);
+        }
+
+        public async Task<FullReportAnalysis> FullAnalysis(CancellationToken cancellationToken, bool enablePolling)
+        {
             Func<Common.FullReportAnalysisResponse.DataTableAnalysisDto, DataTableAnalysis> createDataTable = t => new DataTableAnalysis(t.DataTableName, t.ColumnNames);
-            var dto = await ServerRequest<object, Common.FullReportAnalysisResponse>(null, "/analyzer", _report, cancellationToken);
+            var dto = await ServerRequest<object, Common.FullReportAnalysisResponse>(null, "/analyzer", _report, cancellationToken, enablePolling);
             return new FullReportAnalysis(
                 dto.Parameters,
                 dto.DataTables.Select(createDataTable),
@@ -58,17 +70,16 @@ namespace Majorsilence.CrystalCmd.Client
                 dto.ReportObjects);
         }
 
-        public async Task<Common.FullReportAnalysisResponse> Analyze(CancellationToken cancellationToken = default)
+        public async Task<Common.FullReportAnalysisResponse> Analyze(CancellationToken cancellationToken, bool enablePolling)
         {
-            var dto = await ServerRequest<object, Common.FullReportAnalysisResponse>(null, "/analyzer", _report, cancellationToken);
+            var dto = await ServerRequest<object, Common.FullReportAnalysisResponse>(null, "/analyzer", _report, cancellationToken, false);
             return dto;
         }
 
-        private async Task<U> ServerRequest<T, U>(T reportRequest, string analyzerEndPoint, Stream report, CancellationToken cancellationToken)
+        private async Task<U> ServerRequest<T, U>(T reportRequest, string analyzerEndPoint, Stream report, CancellationToken cancellationToken, bool enablePolling)
         {
-            var serverCaller = new ServerCaller(_client, _baseUrl);
-            using (var response = await serverCaller.GenerateAsync(reportRequest, report, analyzerEndPoint,
-                _username, _password, _userAgent, _bearerToken, cancellationToken))
+            using (var response = await GenerateAsync(reportRequest, report, analyzerEndPoint, enablePolling,
+                cancellationToken))
             {
                 using (var responseReader = new StreamReader(response))
                 {
@@ -77,5 +88,157 @@ namespace Majorsilence.CrystalCmd.Client
                 }
             }
         }
+
+        private async Task<Stream> GenerateAsync<T>(T requestData, Stream report,
+            string endPoint, bool enablePolling = true,
+            System.Threading.CancellationToken cancellationToken = default)
+        {
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
+
+            HttpStatusCode uploadResponse;
+            string uploadId;
+
+            using (var form = new MultipartFormDataContent())
+            {
+                form.Add(new StringContent(json, System.Text.Encoding.UTF8, "application/json"), typeof(T).Name);
+                form.Add(new StreamContent(report), "reporttemplate", "report.rpt");
+
+                var request = new HttpRequestMessage()
+                {
+                    Content = form,
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri(_baseUrl + endPoint)
+                };
+
+                if (!string.IsNullOrWhiteSpace(_bearerToken))
+                {
+                    request.Headers.Add("Authorization", $"Bearer {_bearerToken}");
+                }
+                else if (!string.IsNullOrWhiteSpace(_username) && !string.IsNullOrWhiteSpace(_password))
+                {
+                    request.Headers.Add("Authorization", $"Basic {Base64Encode($"{_username}:{_password}")}");
+                }
+                request.Headers.Add("User-Agent", _userAgent);
+
+                HttpResponseMessage response = await _client.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+                if (enablePolling)
+                {
+                    var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    uploadId = content;
+                    uploadResponse = response.StatusCode;
+                    string errorMessage = "";
+                    try
+                    {
+                        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                        {
+                            errorMessage = content;
+                        }
+                        response.EnsureSuccessStatusCode();
+                    }
+                    catch (HttpRequestException hrex)
+                    {
+                        throw new HttpRequestException(errorMessage, hrex);
+                    }
+
+                    if (uploadResponse == HttpStatusCode.OK)
+                    {
+                        return await PollGet(uploadId, _client, cancellationToken);
+                    }
+
+                    throw new CrystalCmdException("Failed to upload and analyze report file");
+                }
+                else
+                {
+                    var content = response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                    string errorMessage = "";
+                    try
+                    {
+                        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                        {
+                            using (var x = new StreamReader(await content))
+                            {
+                                errorMessage = x.ReadToEnd();
+                            }
+                        }
+                        response.EnsureSuccessStatusCode();
+                    }
+                    catch (HttpRequestException hrex)
+                    {
+                        throw new HttpRequestException(errorMessage, hrex);
+                    }
+
+                    return await content;
+                }
+            }
+        }
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        private async Task<Stream> PollGet(string id, HttpClient httpClient,
+           System.Threading.CancellationToken cancellationToken)
+        {
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(_userAgent);
+            int pollTimeoutInSeconds = 600;
+            var startTime = DateTime.UtcNow;
+            while ((DateTime.UtcNow - startTime).TotalSeconds < pollTimeoutInSeconds)
+            {
+                using (var request = new System.Net.Http.HttpRequestMessage())
+                {
+                    request.Method = new System.Net.Http.HttpMethod("GET");
+                    request.Headers.Add("id", id);
+                    request.RequestUri = new Uri($"{_baseUrl}/analyzer/poll");
+
+                    if (!string.IsNullOrWhiteSpace(_bearerToken))
+                    {
+                        request.Headers.Add("Authorization", $"Bearer {_bearerToken}");
+                    }
+                    else if (!string.IsNullOrWhiteSpace(_username) && !string.IsNullOrWhiteSpace(_password))
+                    {
+                        request.Headers.Add("Authorization", $"Basic {Base64Encode($"{_username}:{_password}")}");
+                    }
+
+                    var response = await httpClient.SendAsync(request, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+
+                    var content = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                    string errorMessage = "";
+                    try
+                    {
+                        if ((int)response.StatusCode > 299 || (int)response.StatusCode < 200)
+                        {
+                            using (var x = new StreamReader(content))
+                            {
+                                errorMessage = await x.ReadToEndAsync();
+                            }
+                            response.EnsureSuccessStatusCode();
+                        }
+                        else if (response.StatusCode == HttpStatusCode.Accepted)
+                        {
+                            // still processing, do nothing  
+                        }
+                        else if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            // processing finished, copy stream to memory stream to avoid disposed problem in caller
+                            var resultStream = new MemoryStream();
+                            await content.CopyToAsync(resultStream);
+                            resultStream.Position = 0;
+                            return resultStream;
+                        }
+                    }
+                    catch (HttpRequestException hrex)
+                    {
+                        throw new HttpRequestException(errorMessage, hrex);
+                    }
+                }
+                await Task.Delay(1000, cancellationToken);
+            }
+
+            throw new CrystalCmdException("Polling timed out");
+        }
+
+
     }
 }
