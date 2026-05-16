c669a3e05b7c84eb167c076f93cccd98649fdbbf
Peter Gill <peter@majorsilence.com>
2026-05-15 22:46:30 -0230

    Add build script for obsolete components and integrate into CI workflow

---
+++ dotnet/build_obsolete.ps1
@@ -0,0 +1,35 @@
+#!/usr/bin/env pwsh
+
+$ErrorActionPreference = "Stop"
+$CURRENTPATH = $pwd.Path
+$MSBUILD = "C:\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
+If (Test-Path "C:\BuildTools\MSBuild\Current\Bin\MSBuild.exe") {
+	$MSBUILD = "C:\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
+}
+ElseIf (Test-Path "C:\Program Files\Microsoft Visual Studio\18\Insiders\MSBuild\Current\Bin\MSBuild.exe") {
+	$MSBUILD = "C:\Program Files\Microsoft Visual Studio\18\Insiders\MSBuild\Current\Bin\MSBuild.exe"
+}
+ElseIf (Test-Path "C:\BuildTools\MSBuild\17.0\Bin\MSBuild.exe") {
+	$MSBUILD = "C:\BuildTools\MSBuild\17.0\Bin\MSBuild.exe"
+}
+ElseIf (Test-Path "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe") {
+	$MSBUILD = "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+}
+else {
+	$MSBUILD="msbuild"
+}
+
+
+cd "$CURRENTPATH\Majorsilence.CrystalCmd.NetFrameworkServer"
+
+
+& "$MSBUILD" "Majorsilence.CrystalCmd.Server.Common\Majorsilence.CrystalCmd.Server.Common.csproj" -maxcpucount /verbosity:minimal /property:Configuration="Release" /target:clean /target:rebuild /p:UseOldCrystalReportsReferences="yes"
+if ($LastExitCode -ne 0) { throw "Building solution, NetFrameworkServer, failed" }
+
+
+Write-Output "Copying nuget packages"
+Get-ChildItem -Recurse "$CURRENTPATH\*Obsolete*.nupkg" | Where-Object { $_.FullName -notmatch '\\packages\\' } | Copy-Item -Destination  "$CURRENTPATH/build"
+
+
+
+cd $CURRENTPATH
