896fb4f70cff90109838b5ff3513936c2b58ff4b
Peter Gill <peter.gill@townsuite.com>
2026-09-01 14:07:54 -0230

    Skip container-backed work queue tests when Docker is unavailable

--- dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.WorkQueues.IntegrationTests/WorkQueuePostgreSqlTests.cs
+++ dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.WorkQueues.IntegrationTests/WorkQueuePostgreSqlTests.cs
@@ -1,14 +1,23 @@
-﻿namespace Majorsilence.CrystalCmd.WorkQueues.IntegrationTests;
+namespace Majorsilence.CrystalCmd.WorkQueues.IntegrationTests;
 
 [TestFixture]
 [Category("Integration")]
 [Category("PostgreSql")]
 public class WorkQueuePostgreSqlTests : WorkQueueTestBase
 {
+    [OneTimeSetUp]
+    public void RequireContainer()
+    {
+        if (ContainerSetup.PostgreSql is null)
+        {
+            Assert.Ignore($"PostgreSQL container is not available on this host: {ContainerSetup.PostgreSqlUnavailableReason}");
+        }
+    }
+
     protected override WorkQueue CreateQueue(string channel, int leaseMinutes)
     {
         var sqlDefs = new WorkQueueSqlDefs(SqlType.PostgreSQL);
         return new WorkQueue(sqlDefs, SqlType.PostgreSQL,
-            ContainerSetup.PostgreSql.GetConnectionString(), channel, leaseMinutes);
+            ContainerSetup.PostgreSql!.GetConnectionString(), channel, leaseMinutes);
     }
 }
--- dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.WorkQueues.IntegrationTests/WorkQueueSqlServerTests.cs
+++ dotnet/Majorsilence.CrystalCmd.NetFrameworkServer/Majorsilence.CrystalCmd.WorkQueues.IntegrationTests/WorkQueueSqlServerTests.cs
@@ -5,10 +5,19 @@
 [Category("SqlServer")]
 public class WorkQueueSqlServerTests : WorkQueueTestBase
 {
+    [OneTimeSetUp]
+    public void RequireContainer()
+    {
+        if (ContainerSetup.SqlServer is null)
+        {
+            Assert.Ignore($"SQL Server container is not available on this host: {ContainerSetup.SqlServerUnavailableReason}");
+        }
+    }
+
     protected override WorkQueue CreateQueue(string channel, int leaseMinutes)
     {
         var sqlDefs = new WorkQueueSqlDefs(SqlType.SqlServer);
         return new WorkQueue(sqlDefs, SqlType.SqlServer,
-            ContainerSetup.SqlServer.GetConnectionString(), channel, leaseMinutes);
+            ContainerSetup.SqlServer!.GetConnectionString(), channel, leaseMinutes);
     }
 }
